781baab4525db0d693393702e63ffe2c5d756d34
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-01 22:33:28 +0200

    fixed image problem

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 // ! Preliminary operations
 
-//  recupero prev e next
+//  I get prev and next
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
+
+// I get the images from the DOM
+const images = document.querySelectorAll('#carousel img');
+
+// I get the gallery from the DOM
+const gallery = document.querySelector('.gallery');
+
+// preparing the images
+const sources = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
+
+// preparing HTML for the images
+let imageElements = '';
+
+//for every source i create a tag img
+for (let i = 0; i < sources.length; i++) {
+    imageElements += `<img src=" ${sources[i]}" alt="Videogame ${i + 1}"> `;
+}
+
+// I insert the images into the DOM
+gallery.innerHTML = imageElements;
 
 
 // Preparing Index
 let currentIndex = 0;
+console.log (currentIndex);
 
 // setting up the first image as active 
 images[currentIndex].classList.add('active');
-
+console.log ('immagini');
 // ! Reasoning logic
 
 // i set next to listen
 nextButton.addEventListener('click', function() {
-
-    // I remove the active cklass from the image that corresponds to currentIndex
+    // if at last image i stop the function
+    if (!currentIndex) return;
+    
+    // I remove the active class from the image that corresponds to currentIndex
     images[currentIndex].classList.remove('active');
 
     // incrementing the current index
@@ -27,7 +50,10 @@ nextButton.addEventListener('click', function() {
 });
 
 // setting prev to listen
-prevButton.addEventListener('click', function(){
+prevButton.addEventListener('click', function(){    
+    // if at last image i block the function
+    if (!currentIndex) return;
+
     // I remove the active class from the corresponding currentIndex image
     images[currentIndex].classList.remove('active');
 
